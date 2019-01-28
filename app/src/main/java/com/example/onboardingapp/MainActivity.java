1d9ee6fdbaf3e447d3da5c7866e1731009281232
Rishabh-More <43803144+Rishabh-More@users.noreply.github.com>
2019-01-28 10:20:35 +0530

    Demo of Intro Slides

---
+++ app/src/main/java/com/example/onboardingapp/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.onboardingapp;
+
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int FROM_HTML_MODE_LEGACY = 0;
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotLayout;
+
+    private TextView[] mDots;
+
+    private SliderAdapter sliderAdapter;
+
+    private Button mNextBtn;
+    private Button mBackBtn;
+    private int mCurrentPage;
+
+    @SuppressWarnings("deprecation")
+    public static Spanned fromHtml (String html){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
+        } else {
+            return Html.fromHtml(html);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSlideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+        mDotLayout = (LinearLayout) findViewById(R.id.dotsLayout);
+
+        mNextBtn = (Button) findViewById(R.id.nextBtn);
+        mBackBtn = (Button) findViewById(R.id.prevBtn);
+
+        sliderAdapter = new SliderAdapter(this);
+
+        mSlideViewPager.setAdapter(sliderAdapter);
+
+        addDotsIndicator(0);
+
+        mSlideViewPager.addOnPageChangeListener(viewListener);
+
+        //OnClickListeners
+        mNextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSlideViewPager.setCurrentItem(mCurrentPage + 1);
+            }
+        });
+
+        mBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSlideViewPager.setCurrentItem(mCurrentPage - 1);
+            }
+        });
+
+    }
+    public void addDotsIndicator(int position){ mDots = new TextView[3];
+        mDotLayout.removeAllViews();
+
+        for(int i=0; i<mDots.length; i++){
+
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8266", Html.FROM_HTML_MODE_COMPACT));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(ContextCompat.getColor(getApplicationContext(),R.color.colorPrimaryDark));
+
+            mDotLayout.addView(mDots[i]);
+        }
+
+        if(mDots.length > 0){
+            mDots[position].setTextColor(ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+            addDotsIndicator(i);
+
+            mCurrentPage = i;
+
+            if(i==0){
+                mNextBtn.setEnabled(true);
+                mBackBtn.setEnabled(false);
+                mBackBtn.setVisibility(View.INVISIBLE);
+
+                mNextBtn.setText("Next");
+                mBackBtn.setText("");
+            }else if(i == mDots.length - 1){
+                mNextBtn.setEnabled(true);
+                mBackBtn.setEnabled(true);
+                mBackBtn.setVisibility(View.VISIBLE);
+
+                mNextBtn.setText("Let's Go");
+                mBackBtn.setText("Back");
+            }else {
+                mNextBtn.setEnabled(true);
+                mBackBtn.setEnabled(true);
+                mBackBtn.setVisibility(View.VISIBLE);
+
+                mNextBtn.setText("Next");
+                mBackBtn.setText("Back");
+            }
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    };
+}
